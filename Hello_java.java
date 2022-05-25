ccdf1f5e45c8ed23228cb82da5a512b9f425645d
reutmarkels <105644327+reutmarkels@users.noreply.github.com>
2022-05-25 12:18:13 +0300

    Create Hello_java.java

---
+++ Hello_java.java
@@ -0,0 +1,7 @@
+
+public class Hello{
+  public static void main (String[]) args {
+    //prints "Hello all, good morning**" in the terminal window
+    system.out.println("Hello all, good morning, we are learning Jenkins-GitHub");
+  }
+}
