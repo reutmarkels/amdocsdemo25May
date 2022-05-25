b28f7038ee7ca40ea158f7e690ef6aa1ee5848ec
reutmarkels <105644327+reutmarkels@users.noreply.github.com>
2022-05-25 12:49:52 +0300

    Update Hello_java.java

--- Hello_java.java
+++ Hello_java.java
@@ -1,5 +1,5 @@
 
-public class Hello{
+public class Hello_java{
   public static void main (String[] args) {
     //prints "Hello all, good morning**" in the terminal window
     system.out.println("Hello all, good morning, we are learning Jenkins-GitHub");
